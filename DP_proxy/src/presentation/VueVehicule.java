d764db681f71debc4f30819e44b6ae63df1fa4d3
emartin6989 <emartin6989@gmail.com>
2017-04-24 17:15:34 +0200

    Exemple de DP

---
+++ DP_proxy/src/presentation/VueVehicule.java
@@ -0,0 +1,15 @@
+package presentation;
+import metier.Animation;
+import metier.AnimationProxy;
+
+public class VueVehicule
+{
+
+  public static void main(String[] args)
+  {
+    Animation animation = new AnimationProxy();
+    animation.dessine();
+    animation.clic();
+    animation.dessine();
+  }
+}
